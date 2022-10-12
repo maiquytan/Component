553ddde4a288aaf1f26559aae4277d19d3e1fdcf
maiquytan <113486856+maiquytan@users.noreply.github.com>
2022-10-12 10:27:51 +0700

    fix confirm and css

--- component/src/Components/DangKi.js
+++ component/src/Components/DangKi.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Dangki() {
+  const [userName, setUserName] = useState ();
+  const [userPass, setUserPass] = useState();
+  const [userConfirm, setUserConfirm] = useState();
+
   return (
     <div className='register'> 
       <h1> Đăng kí </h1>
       <div className='form'>
         <div>
           <label >Usename</label>
-          <input type="text" placeholder='Input username....' required/>
+          <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Input username....' />
         </div>
         <div>
         <label >Password</label>
-        <input type="password" placeholder='....' required/>
+        <input type="password" value={userPass} onChange={(e)=> setUserPass(e.target.value)} placeholder='....' />
         </div>  
         <div>
         <label >Confirm </label>
-        <input type="password" placeholder='....' required/>
+        <input type="password" value={userConfirm} onChange={(e)=> setUserConfirm(e.target.value)} placeholder='....' />
         </div>
         <div>
         <button type='submit' onClick={ notification} className="button"> Đăng kí </button>
@@ -24,8 +28,10 @@ export default function Dangki() {
     </div>
   )
   function notification(){
-    return(
+    if(userName.length>0 && userPass.length>0 && userConfirm.length>0 && userPass===userConfirm){
       alert("Đăng kí thành công")
-    ) 
+    }else{
+      alert("Đăng kí không thành công")
+    }
     }
 }
