ee559a8d13ca2dde827a8adb84ee545a5eb32d53
maiquytan <113486856+maiquytan@users.noreply.github.com>
2022-10-21 11:47:25 +0700

    Add focus usename

--- component/src/Components/DangKi.js
+++ component/src/Components/DangKi.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+// import { ReactDOM } from 'react';
 export default function Dangki() {
   const [userName, setUserName] = useState ('');
   const [userPass, setUserPass] = useState('');
   const [userConfirm, setUserConfirm] = useState('');
-
+  
+  useEffect(() => {
+    document.getElementById("usename").focus();
+  },[])
   return (
     <div className='register'> 
       <h1> Đăng kí </h1>
       <div className='form'>
         <div>
           <label >Usename</label>
-          <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Input username....' />
+          <input id="usename" type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Input username....' />
         </div>
         <div>
         <label >Password</label>
@@ -22,7 +25,7 @@ export default function Dangki() {
         <input type="password" value={userConfirm} onChange={(e)=> setUserConfirm(e.target.value)} placeholder='....' />
         </div>
         <div>
-        <button type='submit' onClick={ notification} className="button"> Đăng kí </button>
+        <button type='submit' onClick={ notification}  className="button"> Đăng kí </button>
         </div>
       </div>
     </div>
